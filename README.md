4d27af9f722cbf8ca2bdf7b75184f66d545ef6d0
Dhruv Patel <dhruv850.dp@gmail.com>
2021-02-01 00:26:56 -0500

    server of MERNG app

---
+++ README.md
@@ -0,0 +1,33 @@
+# MERNG Stack App
+
+## Tech Used
+
+* MongoDB
+* Express
+* React (w/ React hooks API)
+* Node.js
+* Apollo
+* GraphQL
+* JSON Web Tokens
+* Netlify
+* Heroku
+* Semantic UI
+* CSS Grid
+* Moment.js
+
+## Features
+
+* Users can sign up
+* Users can log in
+* Users can create/delete/read posts
+* Users can like and comment on posts
+* Authentication middleware
+* GraphQL server
+* Single Page App (React)
+* Production app deployed on Heroku/Netlify
+
+## Credits
+
+Huge thanks to [@hidjou](https://github.com/hidjou) for creating such a thorough, practical, informative, and educational YouTube series (not to mention it's totally free!)
+
+Check it out on his [YouTube channel](https://youtu.be/n1mdAPFq2Os).
---
+++ index.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+const { ApolloServer, PubSub } = require('apollo-server');
+const mongoose = require('mongoose');
+require('dotenv').config()
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // so we can access the request body in the context, so we can do stuff like checking for authentication in protected routes
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
